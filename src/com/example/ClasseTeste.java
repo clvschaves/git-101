840c389d5fc3d619851254fe5bf670e961aa3d9a
Cloves <clvschaves@gmail.com>
2016-09-15 11:37:28 -0300

    implementação de um Switch

--- src/com/example/ClasseTeste.java
+++ src/com/example/ClasseTeste.java
@@ -1,9 +1,30 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class ClasseTeste {
 	
 	public static void main(String[] args) {
+		
+		String resposta;
 		System.out.println("Main method");
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Aprederam git? S ou  N");
+		resposta = s.nextLine();
+		
+		switch (resposta) {
+		case "s":
+			System.out.println("parabens APRENDEU");
+			break;
+		case "n":
+			System.out.println("Presta atenção");
+		break;
+		default:
+			System.out.println("Pedi para digitar apenas S ou N");
+			break;
+		}
+		
 	}
 
 }
